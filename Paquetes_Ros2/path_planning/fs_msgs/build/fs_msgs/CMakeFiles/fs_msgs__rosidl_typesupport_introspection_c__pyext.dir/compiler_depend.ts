4c24480a6d6b4154ba945d5adcc67e41d28d7295
Jaime Perez Gil <jaimeperezgil21@gmail.com>
2024-07-02 12:17:25 +0200

    gtinore

--- Paquetes_Ros2/path_planning/fs_msgs/build/fs_msgs/CMakeFiles/fs_msgs__rosidl_typesupport_introspection_c__pyext.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for fs_msgs__rosidl_typesupport_introspection_c__pyext.
--- Paquetes_Ros2/path_planning/fs_msgs/build/fs_msgs/ament_cmake_python/fs_msgs/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='fs_msgs',
-    version='0.1.1',
-    packages=find_packages(
-        include=('fs_msgs', 'fs_msgs.*')),
-)
