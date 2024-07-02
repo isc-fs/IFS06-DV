f82d413739fe13731bd5dfe1ef4a74d4966c5472
Coolgolf1 <coolgolfmaster@gmail.com>
2024-07-02 22:24:42 +0200

    setup changes

--- Paquetes_Ros2/odometria/setup.py
+++ Paquetes_Ros2/odometria/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'odometria'
 
@@ -6,20 +8,22 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+    ata_files=[
+        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='lucia',
-    maintainer_email='lucia@todo.todo',
+    maintainer_email='lucia.herraizcano@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'Publicar_TF = odometria.odometria:publicar_tf'
+            'Calcular_Odometria = odometria.odometria:calcular_odometria' 
         ],
     },
 )
