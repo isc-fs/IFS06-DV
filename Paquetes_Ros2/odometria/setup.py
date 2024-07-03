d0ee49bd1dd09ccf5aca3938e4ceac723518fd9f
LuciaHC <lucia.herraizcano@gmail.com>
2024-07-03 11:29:27 +0100

    reinstalación paquete odometria

--- Paquetes_Ros2/odometria/setup.py
+++ Paquetes_Ros2/odometria/setup.py
@@ -1,6 +1,4 @@
 from setuptools import find_packages, setup
-import os
-from glob import glob
 
 package_name = 'odometria'
 
@@ -8,10 +6,10 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    ata_files=[
-        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -22,8 +20,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'Publicar_TF = odometria.odometria:publicar_tf'
-            'Calcular_Odometria = odometria.odometria:calcular_odometria' 
         ],
     },
 )
